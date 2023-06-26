a7ec9d38d939118d1f907d6b2b742bef6ea45e59
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-26 04:56:02 +0300

    Feat: add footer component to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 import TaskPage from './pages/TaskPage.vue'
 </script>
 
 <template>
   <Navbar />
   <TaskPage />
+  <Footer />
 </template>
 
 <style scoped></style>
