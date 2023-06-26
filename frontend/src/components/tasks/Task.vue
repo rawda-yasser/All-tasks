34fa80c8e5814e30aadb48a6adce9b5c320f2bb4
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-26 05:52:28 +0300

    Feat: add completed class dynamically to Task component using computed property

--- frontend/src/components/tasks/Task.vue
+++ frontend/src/components/tasks/Task.vue
@@ -1,8 +1,12 @@
 <template>
   <li class="list-group-item py-3">
     <div class="d-flex justify-content-start align-items-center">
-      <input class="form-check-input mt-0 completed" type="checkbox" />
-      <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+      <input class="form-check-input mt-0" :class="completedClass" type="checkbox" />
+      <div
+        class="ms-2 flex-grow-1"
+        title="Double click the text to edit or remove"
+        :class="completedClass"
+      >
         <!-- <div class="relative">
                                 <input class="editable-task" type="text" />
                             </div> -->
@@ -23,7 +27,9 @@
 <script setup>
 import EditIcon from '../icons/EditIcon.vue'
 import DeleteIcon from '../icons/DeleteIcon.vue'
-defineProps({
+import { computed } from 'vue'
+const props = defineProps({
   task: Object
 })
+const completedClass = computed(() => (props.task.is_completed ? 'completed' : ''))
 </script>
