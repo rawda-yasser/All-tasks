8a08930fda91cf19c3cec1f495b7c5843ddd9b65
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-27 09:56:40 +0300

    Fix: update footer to display all tasks name

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -14,6 +14,6 @@
         <a href="#" class="nav-link px-2 text-muted">About</a>
       </li>
     </ul>
-    <p class="text-center text-muted">&copy; 2023 ToeDoeList</p>
+    <p class="text-center text-muted">&copy; 2023 All Tasks</p>
   </footer>
 </template>
