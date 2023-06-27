74f980c81ce4f53eb5ae947da9b52e3db45fc34f
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-27 07:30:17 +0300

    Refactor: update task store to use arrow function rather than object

--- frontend/src/stores/task.js
+++ frontend/src/stores/task.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import { allTasks } from '../http/task-api'
-export const useTaskStore = defineStore('taskStore', {
+import { reactive, ref, computed } from 'vue'
+const tmp = {
   state: () => ({
     tasks: [],
     task: {
@@ -9,16 +10,17 @@ export const useTaskStore = defineStore('taskStore', {
       is_completed: false
     }
   }),
-  getters: {
-    completedTasks: (state) => state.tasks.filter((task) => task.is_completed),
-    uncompletedTasks() {
-      return this.tasks.filter((task) => !task.is_completed)
-    }
-  },
-  actions: {
-    async fetchAllTasks() {
-      const { data } = await allTasks()
-      this.tasks = data.data
-    }
+  getters: {},
+  actions: {}
+}
+export const useTaskStore = defineStore('taskStore', () => {
+  const tasks = ref([])
+
+  const uncompletedTasks = computed(() => tasks.value.filter((task) => !task.is_completed))
+  const completedTasks = computed(() => tasks.value.filter((task) => task.is_completed))
+  const fetchAllTasks = async () => {
+    const { data } = await allTasks()
+    tasks.value = data.data
   }
+  return { tasks, completedTasks, uncompletedTasks, fetchAllTasks }
 })
