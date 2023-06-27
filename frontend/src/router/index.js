5a5a49f077c7a2b76e03d2bdab0d5b99cdf1cf6c
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-27 10:16:38 +0300

    Feat: add a router.beforeEach function setup with a log message

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
-export default createRouter({
+const router = createRouter({
   routes,
   history: createWebHistory(),
   linkActiveClass: 'active'
 })
+router.beforeEach(() => {
+  console.log('Global before each')
+})
+export default router
